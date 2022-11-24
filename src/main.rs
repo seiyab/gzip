b3df419fa76fcf35bf54d17a4c726f42c35d12ff
seiya <20365512+seiyab@users.noreply.github.com>
2022-11-25 08:41:42 +0900

    print stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, Write};
+
+fn main() -> io::Result<()> {
+    let mut buf: [u8; 10] = [0; 10];
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+    loop {
+        let n = stdin.read(&mut buf)?;
+        if n == 0 {
+            break;
+        };
+        stdout.write(&buf[0..n])?;
+    }
+
+    Ok(())
 }
