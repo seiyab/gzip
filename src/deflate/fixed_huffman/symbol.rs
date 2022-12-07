f5ab4b575029d9ab65f692cd0c5a620dc19bc22b
seiya <20365512+seiyab@users.noreply.github.com>
2022-12-07 23:17:44 +0900

    support distance <= 114

--- src/deflate/fixed_huffman/symbol.rs
+++ src/deflate/fixed_huffman/symbol.rs
@@ -7,7 +7,7 @@ pub enum Symbol {
 }
 
 impl Symbol {
-    pub const MAX_LENGTH: usize = 10;
+    pub const MAX_LENGTH: usize = 114;
 
     pub fn encode(&self) -> Vec<bool> {
         match self {
@@ -31,11 +31,30 @@ fn usize_to_bits(x: usize, len: usize) -> Vec<bool> {
     (0..len).map(|i| ((x >> i) & 1) > 0).rev().collect()
 }
 
+fn u32_to_bits(x: u32, len: u32) -> Vec<bool> {
+    (0..len).map(|i| ((x >> i) & 1) > 0).rev().collect()
+}
+
+fn u32_to_rev_bits(x: u32, len: u32) -> Vec<bool> {
+    (0..len).map(|i| ((x >> i) & 1) > 0).collect()
+}
+
 fn length_code(l: usize) -> Vec<bool> {
-    if l <= Symbol::MAX_LENGTH {
+    if l < 11 {
         return usize_to_bits(257 - 3 + l, 7);
     }
-    panic!("unsupported length");
+    if l > Symbol::MAX_LENGTH {
+        panic!("unsupported length")
+    }
+    let extra_bits_len = 64 - (l - 3).leading_zeros() - 3;
+    let group_min_length = (1u32 << (extra_bits_len + 2)) + 3;
+    let group_min_code = 261 + (extra_bits_len * 4);
+    let size_in_group = l as u32 - group_min_length;
+    let code = group_min_code + (size_in_group >> extra_bits_len);
+    let mut bits = u32_to_bits(code, 7);
+    let extra_bits = u32_to_rev_bits(size_in_group, extra_bits_len);
+    bits.extend(extra_bits);
+    return bits;
 }
 
 fn distance_code(d: usize) -> Vec<bool> {
