8be65eaf1cc404de8badc7af8e5dd329ad4a5ae4
seiya <20365512+seiyab@users.noreply.github.com>
2023-01-08 16:55:40 +0900

    refactor crc

--- src/gzip/crc.rs
+++ src/gzip/crc.rs
@@ -1,21 +1,35 @@
-pub fn crc(data: &Vec<u8>) -> [u8; 4] {
-    let mut c: usize = 0xff_ff_ff_ff;
-    for &d in data.iter() {
-        c = TABLE[(c ^ d as usize) & 0xff] ^ (c >> 8);
-    }
-    c = c ^ 0xff_ff_ff_ff;
-
-    let mut cs: [u8; 4] = [0; 4];
-    for (i, &b) in c.to_le_bytes().iter().take(4).enumerate() {
-        cs[i] = b
-    }
-    cs
+pub struct Crc {
+    value: u32,
 }
 
-const TABLE: [usize; 256] = make_table();
+impl Crc {
+    pub fn new() -> Self {
+        Self {
+            value: 0xff_ff_ff_ff,
+        }
+    }
 
-const fn make_table() -> [usize; 256] {
-    let mut t: [usize; 256] = [0; 256];
+    pub fn append(self, data: &[u8]) -> Self {
+        let mut c = self.value;
+        for &d in data.iter() {
+            c = TABLE[((c ^ u32::from(d)) & 0xff) as usize] ^ (c >> 8);
+        }
+        Self { value: c }
+    }
+
+    pub fn get(&self) -> [u8; 4] {
+        (self.value ^ 0xff_ff_ff_ff).to_le_bytes()
+    }
+}
+
+pub fn crc(data: &Vec<u8>) -> [u8; 4] {
+    Crc::new().append(data).get()
+}
+
+const TABLE: [u32; 256] = make_table();
+
+const fn make_table() -> [u32; 256] {
+    let mut t: [u32; 256] = [0; 256];
     let mut n: usize = 0;
     while n < 256 {
         t[n] = table_elem(n);
@@ -24,8 +38,8 @@ const fn make_table() -> [usize; 256] {
     return t;
 }
 
-const fn table_elem(n: usize) -> usize {
-    let mut c = n;
+const fn table_elem(n: usize) -> u32 {
+    let mut c = n as u32;
     let mut i: usize = 0;
     while i < 8 {
         if c & 1 > 0 {
