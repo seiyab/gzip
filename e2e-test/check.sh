5af57fa99ba4ac5c1fb0ad7172913e280fe345ed
seiya <20365512+seiyab@users.noreply.github.com>
2023-01-08 13:57:24 +0900

    compressing file

--- e2e-test/check.sh
+++ e2e-test/check.sh
@@ -14,7 +14,9 @@ FILES=$( ls $DATA_DIR )
 for FILE in $FILES
 do
     TEMP_FILE="$SCRIPT_DIR/temp"
-    cat "$DATA_DIR/$FILE" | $BIN | gunzip - > "$TEMP_FILE"
+    "$BIN" "$DATA_DIR/$FILE"
+    gunzip -c "$DATA_DIR/$FILE.gz" > "$TEMP_FILE"
+    rm "$DATA_DIR/$FILE.gz" 
     diff "$DATA_DIR/$FILE" "$TEMP_FILE"
     rm "$TEMP_FILE"
 done
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use chrono::Local;
-use std::io::{self, Read, Write};
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+};
 
 mod args;
 mod deflate;
@@ -8,11 +11,15 @@ mod gzip;
 fn main() -> io::Result<()> {
     let mut buf = Vec::<u8>::new();
     let a = args::Args::parse().unwrap();
-    let (mut input, mut output) = if let Some(_) = a.filepath {
-        todo!();
-    } else {
-        (io::stdin(), io::stdout())
-    };
+    let (mut input, mut output): (Box<dyn Read>, Box<dyn Write>) =
+        if let Some(filepath) = a.filepath {
+            (
+                Box::new(File::open(&filepath)?),
+                Box::new(File::create(filepath + ".gz")?),
+            )
+        } else {
+            (Box::new(io::stdin()), Box::new(io::stdout()))
+        };
     input.read_to_end(&mut buf)?;
 
     let gzipped = gzip::gzip(&buf, &Local::now());
